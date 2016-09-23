da7fc8a4a41341d295c8ce6cb476e8f877f11e0e
ksa607 <karine.samyn@hogent.be>
2016-09-23 18:37:58 +0200

    Creatie Banking applicatie met fouten

---
+++ src/Banking/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Banking.Models;
+
+namespace Banking
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("123-4567890-02");
+            Console.WriteLine($"AccountNumber: {account.AccountNumber} ");
+            Console.WriteLine($"Balance: {account.Balance} ");
+            account.Deposit(200M);
+            Console.WriteLine($"Balance after deposit of 200 euros: {account.Balance} ");
+            Console.WriteLine($"Number of transactions after deposit of 200 euros: {account.NumberOfTransactions} ");
+            account.Withdraw(100M);
+            Console.WriteLine($"Balance after withdraw 100 euros: {account.Balance} ");
+            Console.WriteLine($"Number of transactions after withdraw of 100 euros: {account.NumberOfTransactions} ");
+            int aantal = (account.GetTransactions(DateTime.Today.AddDays(-2), DateTime.Today)).Count();
+            Console.WriteLine($"Aantal transacties: {aantal}");
+
+            BankAccount savingsAccount = new SavingsAccount("123-4567890-02", 0.05M);
+            Console.WriteLine($"SavingsAccount : {savingsAccount}");
+            savingsAccount.Deposit(200M);
+            savingsAccount.Withdraw(100M);
+            Console.WriteLine($"Balance savingsaccount: {savingsAccount.Balance} ");
+            Console.ReadKey();
+        }
+    }
+}
